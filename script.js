2f62be7260f4a0be42eb11476c9810b3ff440b34
alevanfof <afernandez@iset55.edu.ar>
2025-02-25 13:51:41 -0300

    finalización app

--- script.js
+++ script.js
@@ -16,5 +16,38 @@ document.getElementById('searchButton').addEventListener('click', () => {
 function fetchWeather(city){
     fetch(`${urlBase}?q=${city}&appid=${API_KEY}&lang=es`)
     .then(data => data.json())
-    .then(data => console.log(data))
+    .then(data => showWeatherData(data))
+}
+
+function showWeatherData(data){
+    const divResponseData = document.getElementById('responseData')
+    divResponseData.innerHTML = ''
+
+    const cityName = data.name
+    const countryName = data.sys.country
+    const temp = data.main.temp
+    const humidity = data.main.humidity
+    const description = data.weather[0].description
+    const icon = data.weather[0].icon
+
+    const cityInfo = document.createElement('h2')
+    cityInfo.textContent = `${cityName}, ${countryName}`
+
+    const tempInfo = document.createElement('p')
+    tempInfo.textContent = `La temperatura es: ${Math.floor(temp-diffKelvin)}ºC`
+
+    const humidityInfo = document.createElement('p')
+    humidityInfo.textContent = `La humedad es del ${humidity}%`
+
+    const icoInfo = document.createElement('img')
+    icoInfo.src = `https://openweathermap.org/img/wn/${icon}@2x.png` // verificar en la documentación como mostrar una imagen
+
+    const descriptionInfo = document.createElement('p')
+    descriptionInfo.textContent = `La descripción meteorológica es: ${description}`
+
+    divResponseData.appendChild(cityInfo)
+    divResponseData.appendChild(tempInfo)
+    divResponseData.appendChild(humidityInfo)
+    divResponseData.appendChild(icoInfo)
+    divResponseData.appendChild(descriptionInfo)
 }
